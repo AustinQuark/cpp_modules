03ea2205a8ecea48e025ecbc68d4dab993fcbfb2
AustinFlap <austin.vieira@live.fr>
2021-11-05 14:57:08 +0100

    cpp01 ex03

--- cpp_01/exo3/sources/main.cpp
+++ cpp_01/exo3/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:38:20 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/05 14:45:11 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/05 14:56:37 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int main(void)
     {
         Weapon club = Weapon("crude spiked club");
         HumanA bob("Bob", club);
+        Weapon axe = Weapon("axe");
         bob.attack();
         club.setType("some other type of club");
         bob.attack();
