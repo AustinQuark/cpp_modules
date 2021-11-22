5a1ff3ea01577dc852693ff20f1ffd317180da6b
AustinFlap <austin.vieira@live.fr>
2021-11-23 00:08:52 +0100

    cpp07 ex00

---
+++ cpp_07/ex00/headers/Functions.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Functions.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 23:57:28 by avieira           #+#    #+#             */
+/*   Updated: 2021/11/23 00:06:43 by avieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_HPP
+# define FUNCTIONS_HPP
+
+template <typename T> void swap(T &a, T &b)
+{
+    T temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T> T min(T &a, T &b)
+{
+    if (a < b)
+        return (a);
+    return (b);
+}
+
+template <typename T> T max(T &a, T &b)
+{
+    if (a > b)
+        return (a);
+    return (b);
+}
+
+#endif
