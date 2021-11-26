ca0fcd5916f59c79c1040caa802e3de50bd40a81
AustinFlap <austin.vieira@live.fr>
2021-11-27 00:45:01 +0100

    cpp07 ex01

--- cpp_07/ex00/sources/main.cpp
+++ cpp_07/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 00:04:02 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/23 00:05:04 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/27 00:05:53 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,41 @@
 
 int main(void)
 {
-    	int			a, b;
+    int			a, b;
  	float		c, d;
  	char		e, f;
  	std::string g, h;
  
- 	a = 1;
- 	b = 2;
- 	c = 4.2f;
- 	d = 42.21f;
- 	e = 'a';
- 	f = 'x';
+ 	a = 0;
+ 	b = 42;
+ 	c = 42.24f;
+ 	d = 24.42f;
+ 	e = '4';
+ 	f = '2';
  	g = "string1";
  	h = "string2";
+	std::cout << "---Values" << std::endl;
+	std::cout << "a: " << a << " and b: " << b << std::endl;
+	std::cout << "c: " << c << " and d: " << d << std::endl;
+ 	std::cout << "e: " << e << " and f: " << f << std::endl;
+ 	std::cout << "g: " << g << " and h: " << h << std::endl;
+ 	std::cout << "---Swap them" << std::endl;
+ 	swap(a, b);
+ 	swap(c, d);
+ 	swap(e, f);
+ 	swap(g, h);
  	std::cout << "a: " << a << " and b: " << b << std::endl;
  	std::cout << "c: " << c << " and d: " << d << std::endl;
  	std::cout << "e: " << e << " and f: " << f << std::endl;
  	std::cout << "g: " << g << " and h: " << h << std::endl;
- 	std::cout << "Now calling swap on all these..." << std::endl;
- 	::swap(a, b);
- 	::swap(c, d);
- 	::swap(e, f);
- 	::swap(g, h);
- 	std::cout << "a: " << a << " and b: " << b << std::endl;
- 	std::cout << "c: " << c << " and d: " << d << std::endl;
- 	std::cout << "e: " << e << " and f: " << f << std::endl;
- 	std::cout << "g: " << g << " and h: " << h << std::endl;
- 	std::cout << "min(a, b): " << ::min(a, b) << std::endl;
- 	std::cout << "max(a, b): " << ::max(a, b) << std::endl;
- 	std::cout << "min(c, d): " << ::min(c, d) << std::endl;
- 	std::cout << "max(c, d): " << ::max(c, d) << std::endl;
- 	std::cout << "min(e, f): " << ::min(e, f) << std::endl;
- 	std::cout << "max(e, f): " << ::max(e, f) << std::endl;
- 	std::cout << "min(g, h): " << ::min(g, h) << std::endl;
- 	std::cout << "max(g, h): " << ::max(g, h) << std::endl;
+	std::cout << "---Min/Max them" << std::endl;
+	std::cout << "min(" << a << ", " << b << "): " << min(a, b) << std::endl;
+	std::cout << "max(" << a << ", " << b << "): " << max(a, b) << std::endl;
+ 	std::cout << "min(" << c << ", " << d << "): " << min(c, d) << std::endl;
+ 	std::cout << "max(" << c << ", " << d << "): " << max(c, d) << std::endl;
+ 	std::cout << "min(" << e << ", " << f << "): " << min(e, f) << std::endl;
+ 	std::cout << "max(" << e << ", " << f << "): " << max(e, f) << std::endl;
+ 	std::cout << "min(" << g << ", " << h << "): " << min(g, h) << std::endl;
+ 	std::cout << "max(" << g << ", " << h << "): " << max(g, h) << std::endl;
+
 }
---
+++ cpp_07/ex01/sources/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 00:04:02 by avieira           #+#    #+#             */
+/*   Updated: 2021/11/27 00:43:58 by avieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/Iter.hpp"
+#include "../../ex00/headers/Functions.hpp"
+
+#include <string>
+#include <iostream>
+
+template <typename T> void displayOneElem(T &c)
+{
+	std::cout << c;
+}
+
+int main(void)
+{
+	std::string text = "Hello, display string char by char";
+
+	iter(text.c_str(), text.length(), &displayOneElem);
+	std::cout << std::endl;
+
+	int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	iter(array, sizeof(array) / sizeof(array[0]), &displayOneElem);
+	std::cout << std::endl;
+	
+}
