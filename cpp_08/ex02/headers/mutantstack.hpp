b7b29f14b85a693bf3f16e462b6d9024652f2dac
Austin VIEIRA <avieira@e3r12p19.clusters.42paris.fr>
2021-11-29 15:58:57 +0100

    clean main

--- cpp_08/ex02/headers/mutantstack.hpp
+++ cpp_08/ex02/headers/mutantstack.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 00:19:52 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/29 01:32:37 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/29 15:57:34 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ class MutantStack : public std::stack<T>
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
         typename std::stack<T>::container_type under_type;
 
-        iterator begin() {return (under_type.begin());}
+        iterator begi() {return (under_type.begin());}
         const_iterator begin() const {return (under_type.begin());}
         iterator end() {return (under_type.end());}
         const_iterator end() const {return (under_type.end());}
         iterator rend() {return (under_type.rend());}
         const_iterator rend() const {return (under_type.rend());}
         
-        iterator &iterator::operator++() {};
+        //iterator &iterator::operator++() {};
 };
 
 #endif
--- cpp_08/ex02/sources/main.cpp
+++ cpp_08/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:36:33 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/29 01:29:15 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/29 15:58:22 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,24 @@ int main(void)
 
     mstack.push(5);
     mstack.push(17);
+
     std::cout << mstack.top() << std::endl;
+
     mstack.pop();
+
     std::cout << mstack.size() << std::endl;
-    mstack.push(3); mstack.push(5); mstack.push(737); //[...] mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
+
+    mstack.push(3); 
+    mstack.push(5); 
+    mstack.push(737); //[...] mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin(); 
+    MutantStack<int>::iterator ite = mstack.end();
+    
     ++it;
     --it;
 
     while (it != ite) {
-        std::cout << *it << std::endl;
-    ++it; }
-    std::stack<int> s(mstack); return 0;
+        std::cout << *it << std::endl; ++it; }
+    std::stack<int> s(mstack); 
+    return 0;
 }
