09a4b7bd68eb1be9801afeb2c17b0fb94053a5fd
Austin VIEIRA <avieira@e3r12p19.clusters.42paris.fr>
2021-11-29 17:22:22 +0100

    rbegin and rend

--- cpp_08/ex02/headers/mutantstack.hpp
+++ cpp_08/ex02/headers/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 00:19:52 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/29 16:23:51 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/29 17:13:06 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ class MutantStack : public std::stack<T>
     public:
         typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
         iterator begin() {return (std::stack<T>::c.begin());}
         const_iterator begin() const {return (std::stack<T>::c.begin());}
         iterator end() {return (std::stack<T>::c.end());}
         const_iterator end() const {return (std::stack<T>::c.end());}
-        iterator rbegin() {return (std::stack<T>::c.rbegin());}
-        const_iterator rbegin() const {return (std::stack<T>::c.rbegin());}
-        iterator rend() {return (std::stack<T>::c.rend());}
-        const_iterator rend() const {return (std::stack<T>::c.rend());}
+        reverse_iterator rbegin() {return (std::stack<T>::c.rbegin());}
+        const_reverse_iterator rbegin() const {return (std::stack<T>::c.rbegin());}
+        reverse_iterator rend() {return (std::stack<T>::c.rend());}
+        const_reverse_iterator rend() const {return (std::stack<T>::c.rend());}
 };
 
 #endif
--- cpp_08/ex02/sources/main.cpp
+++ cpp_08/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:36:33 by avieira           #+#    #+#             */
-/*   Updated: 2021/11/29 16:29:26 by avieira          ###   ########.fr       */
+/*   Updated: 2021/11/29 17:20:03 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,42 @@
 
 #include <iostream>
 
-#define SIZE 42
+#define SIZE 43
 
 int main(void)
 {
     MutantStack<int> mstack;
 
-    std::cout << "Creating stack of ints from 0 to " << SIZE << std::endl;
-    for (int i = 0; i < SIZE; i++)
+    std::cout << "Creating mutantstack of ints from 1 to " << SIZE - 1 << " using push method of stack" << std::endl;
+    for (int i = 1; i < SIZE; i++)
         mstack.push(i);
 
-    std::cout << mstack.top() << std::endl;
-
-
-    std::cout << mstack.size() << std::endl;
-
-    mstack.push(3); 
-    mstack.push(5); 
-    mstack.push(737); //[...] mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin(); 
+    MutantStack<int>::iterator it = mstack.begin();
+    std::cout << "Getting begin iterator it, *it = " << *it << std::endl;
     MutantStack<int>::iterator ite = mstack.end();
-    
-    ++it;
-    --it;
+    std::cout << "Getting end iterator ite, *(ite -  1) = " << *(ite - 1) << std::endl;
 
-    while (it != ite) {
-        std::cout << *it << std::endl; ++it; }
-    std::stack<int> s(mstack); 
+    std::cout << "Displaying mutantstack using while (it != ite){...; ++it;}" << std::endl;
+    while (it != ite)
+    {
+        std::cout << *it << " "; 
+        ++it; 
+    }
+    std::cout << std::endl;
+
+    MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+    std::cout << "Getting rbegin iterator rit, *rit = " << *rit << std::endl;
+    MutantStack<int>::reverse_iterator rite = mstack.rend();
+    std::cout << "Getting rend iterator rite, *(rite -  1) = " << *(rite - 1) << std::endl;
+
+    std::cout << "Displaying mutantstack using while (rit != rite){...; rit + 2;}" << std::endl;
+    while (rit != rite)
+    {
+        std::cout << *rit << " "; 
+        rit += 2; 
+    }
+    std::cout << std::endl;
+
+    std::stack<int> s(mstack);
     return 0;
 }
